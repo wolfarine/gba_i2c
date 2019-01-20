5231475eb567466fece7b543420c6283b2bd1a5d
wolfarine <25995301+wolfarine@users.noreply.github.com>
2019-01-20 20:53:54 +0800

    Update delay.c
    fix timing issue

--- source/delay.c
+++ source/delay.c
@@ -3,18 +3,22 @@
 void delayNOP()
 {
 
-//REG_TM3CNT_H=TIMER_1|TIMER_START;//it seems impossible to use time to do delay several ,including intrwait(1,timer).
-//REG_TM3CNT_L=0;
-//while(REG_TM3CNT_L<cycles);
-//REG_TM3CNT_H=0;
-//delay_10cycles();
-__asm("NOP");//some nop instructions to delay cpu cycles for i2c bus,1 cycle is about 60ns,7 cycles will be 0.4us (400khz).
-__asm("NOP");
-__asm("NOP");
-__asm("NOP");
-__asm("NOP");
-__asm("NOP");
-__asm("NOP");
+	//REG_TM3CNT_H=TIMER_1|TIMER_START;//it seems impossible to use time to do delay several ,including intrwait(1,timer).
+	//REG_TM3CNT_L=0;
+	//while(REG_TM3CNT_L<cycles);
+	//REG_TM3CNT_H=0;
+	//delay_10cycles();
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");__asm("NOP");
+	__asm("NOP");__asm("NOP");
+	//some nop instructions to delay cpu cycles for i2c bus,1 cycle is about 60ns,42 cycles will be 2.5us (400khz).
+
 }
 
 
@@ -22,19 +26,19 @@ __asm("NOP");
 void delayms(int ms)
 {
 
-REG_TM3CNT_H=TIMER_256|TIMER_START;
-REG_TM3CNT_L=0;//clear timer3 for next time
-while(REG_TM3CNT_L<(65*us));//65*256*60ns=0.99ms
-REG_TM3CNT_H=0;//close timer3
+	REG_TM3CNT_H=TIMER_256|TIMER_START;
+	REG_TM3CNT_L=0;//clear timer3 for next time
+	while(REG_TM3CNT_L<(65*ms));//65*256*60ns=0.99ms
+	REG_TM3CNT_H=0;//close timer3
 }
 
-void delayus(int os)
+void delayus(int us)
 {
 
-REG_TM3CNT_H=TIMER_1|TIMER_START;
-REG_TM3CNT_L=0;//clear timer3 for next time
-while(REG_TM3CNT_L<(18*ms));//18*1*60ns=1000ns
-REG_TM3CNT_H=0;//close timer3
+	REG_TM3CNT_H=TIMER_1|TIMER_START;
+	REG_TM3CNT_L=0;//clear timer3 for next time
+	while(REG_TM3CNT_L<(18*us));//18*1*60ns=1000ns
+	REG_TM3CNT_H=0;//close timer3
 }
 
 void delay014ms(u8 x)
